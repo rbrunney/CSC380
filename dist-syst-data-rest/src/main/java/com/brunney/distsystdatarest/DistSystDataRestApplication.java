c91721b45de3f0d5ce7aa09ebed0093f6f0e6865
rbrunney <rbrunney@student.neumont.edu>
2022-01-10 15:18:38 -0700

    Initializing CSC380 Repository

---
+++ dist-syst-data-rest/src/main/java/com/brunney/distsystdatarest/DistSystDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.brunney.distsystdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistSystDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistSystDataRestApplication.class, args);
+    }
+
+}
---
+++ dist-syst-data-rest/src/test/java/com/brunney/distsystdatarest/DistSystDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunney.distsystdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistSystDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
